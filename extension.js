52a198448d1b82d6fa355c4a659f2e747aacf854
Samuel <sagarma_94@hotmail.com>
2021-03-08 17:04:07 +0100

    v8

--- extension.js
+++ extension.js
@@ -82,17 +82,17 @@ async function activate(context) {
 	disposable = vscode.commands.registerCommand('virustotal.analyze_iocs', async function (file_name) {
 		let content = fs.readFileSync(file_name.fsPath,"utf-8")
 		let lines = content.split("\n")
-		let toReturn = ""
+		let toReturn = "IOC\tHarmless\tMalicious\tSuspicious\tUndetected\n"
 		for(let ln of lines){
 			let res = null
 			try{
 				res = VT_CACHE.analyze_data(ln);
 			}catch(e){}
 
-			if(res && res.data && res.data.attributes) {
+			if(res && res.data && res.data.attributes && res.data.attributes.last_analysis_stats) {
 				let malicious = ""
 				try {
-					malicious = res.data.attributes.harmless + " " + res.data.attributes.malicious + " " + res.data.attributes.suspicious + " " + res.data.attributes.undetected
+					malicious = res.data.attributes.last_analysis_stats.harmless + "\t" + res.data.attributes.last_analysis_stats.malicious + "\t" + res.data.attributes.last_analysis_stats.suspicious + "\t" + res.data.attributes.last_analysis_stats.undetected
 				}catch(e){}
 				toReturn += ln + "\t" + malicious + "\n"
 			}else{
